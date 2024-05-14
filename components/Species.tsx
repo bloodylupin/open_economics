2537097fe244d3b38d600065c7e28498998241f2
bloodylupin <andrea.borin.ndr@gmail.com>
2024-05-14 18:41:16 +0200

    working version with species model and Promise Await All

--- components/Species.tsx
+++ components/Species.tsx
@@ -1,12 +1,12 @@
 import { fetchMeasuresById, fetchSpeciesDataByRegion } from "@/actions/actions";
 import { RegionType } from "@/types/type";
-import { Suspense } from "react";
-import Measures from "./Measures";
-import Loading from "./UI/Loading";
+// import { Suspense } from "react";
+// import Measures from "./Measures";
+// import Loading from "./UI/Loading";
 
 export default async function Species({ region }: { region: RegionType }) {
-  // const data = await fetchSpeciesDataByRegion(region.identifier);
-  const data = await fetchSpeciesDataByRegion("europe");
+  const data = await fetchSpeciesDataByRegion(region.identifier);
+  // const data = await fetchSpeciesDataByRegion("europe");
 
   const speciesList =
     data &&
@@ -18,33 +18,49 @@ export default async function Species({ region }: { region: RegionType }) {
       class: data.class_name,
     }));
 
-  // console.log(speciesList);
-
-  // const measures =
-  //   speciesList &&
-  //   (await Promise.all(
-  //     speciesList.map(async (species) => await fetchMeasuresById(species.id))
-  //   ));
-
-  // console.log(measures);
-
   const filteredData =
     speciesList &&
     speciesList
       .filter((species) => species.category === "VU")
       .filter((species) => species.class === "MAMMALIA");
 
+  const measures =
+    filteredData &&
+    (await Promise.all(
+      filteredData.map(async (species) => await fetchMeasuresById(species.id))
+    ));
+
+  const speciesArray =
+    filteredData &&
+    measures &&
+    filteredData.map((data, index) => ({
+      ...data,
+      measures: measures[index]?.result
+        .map((measure) => measure.title)
+        .join(" - "),
+    }));
+
   return (
     <div className="grid grid-cols-3 gap-8">
-      {filteredData && filteredData.length ? (
-        filteredData.map((data) => (
+      {speciesArray && speciesArray.length ? (
+        speciesArray.map((data) => (
           <div key={data.id} className="p-8 shadow shadow-black">
             <h2>{data.name}</h2>
             <h2>{data.class}</h2>
             <h3>Conservation measures</h3>
-            <Suspense fallback={<Loading />}>
+            <ul>
+              {data.measures ? (
+                data.measures
+                  .split(" - ")
+                  .map((data) => <li key={data}>{data}</li>)
+              ) : (
+                <li>no data for this species.</li>
+              )}
+            </ul>
+
+            {/* <Suspense fallback={<Loading />}>
               <Measures id={data.id} />
-            </Suspense>
+            </Suspense> */}
           </div>
         ))
       ) : (
