b4baf98dad2ce11d28e691d80461b51742582931
bloodylupin <andrea.borin.ndr@gmail.com>
2024-05-17 09:39:35 +0200

    purged button test

--- __tests__/FilterButton.test.tsx
+++ __tests__/FilterButton.test.tsx
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { findByRole, render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import FilterButton from "@/components/FilterButton";
 
 describe("FilterButton Features", () => {
@@ -15,22 +15,4 @@ describe("FilterButton Features", () => {
     const button = screen.getByRole("button");
     expect(button.classList[0]).toBe("bg-blue-500");
   });
-
-  //   it("Is darker when hovered", async () => {
-  //     render(<FilterButton />);
-  //     const button = screen.getByRole("button");
-  //     expect(
-  //       window.getComputedStyle(button).getPropertyValue("background-color")
-  //     ).toBe("rgb(59 130 246)");
-
-  //     fireEvent.mouseEnter(button);
-  //     expect(
-  //       window.getComputedStyle(button).getPropertyValue("background-color")
-  //     ).toBe("rgb(29 78 216)");
-
-  //     fireEvent.mouseLeave(button);
-  //     expect(
-  //       window.getComputedStyle(button).getPropertyValue("background-color")
-  //     ).toBe("rgb(59 130 246)");
-  //   });
 });
